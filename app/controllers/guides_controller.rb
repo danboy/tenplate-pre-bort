a99360dc19458b144455ef04920e3038738c7bfa
Dan Nawara <dan@zooey.(none)>
2009-01-22 09:13:26 -0600

    preparing for refactor

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,16 +1,12 @@
 class GuidesController < ApplicationController
-  # GET /guides
-  # GET /guides.xml
   def index
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => "<woot/>" }
     end
   end
+
   def forms
-    respond_to do |format|
-      format.html # forms.html.erb
-      format.xml  { render :xml => "<woot/>" }
-    end
+
   end
 end
